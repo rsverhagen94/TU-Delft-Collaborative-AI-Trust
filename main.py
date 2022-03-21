b71da45ca35560868025797c7952b5699059b639
Max Le Blansch <m.l.leblansch@student.tudelft.nl>
2022-03-21 14:07:42 +0100

    Fixed Colorblind agent

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bw4t.BW4TWorld import BW4TWorld
 from bw4t.statistics import Statistics
 from agents1.BW4TBaselineAgent import BaseLineAgent
 from agents1.BW4THuman import Human
+from agents1.Team36ColorblindAgent import ColorBlindAgent
 
 
 """
@@ -12,7 +13,7 @@ press the start button in god mode to start the session.
 if __name__ == "__main__":
     agents = [
         {'name':'agent1', 'botclass':BaseLineAgent, 'settings':{'slowdown':10}},
-        {'name':'agent2', 'botclass':BaseLineAgent, 'settings':{}},
+        {'name':'agent2', 'botclass':ColorBlindAgent, 'settings':{}},
         {'name':'human', 'botclass':Human, 'settings':{}}
         ]
 
