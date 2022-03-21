4910a9cfaa85a95499669d0227b767e8bbeb8e41
Ana Vasilcoiu <a.m.vasilcoiu@student.tudelft.nl>
2022-03-21 17:14:55 +0100

    Add methods with messages + 2 new phases (if no goal block  to pick up, blind goes to check if agents have picked up objs when they said they did)

--- agents1/BlindAgent.py
+++ agents1/BlindAgent.py
@@ -26,6 +26,9 @@ class Phase(enum.Enum):
     FOLLOW_PATH_TO_GOAL_BLOCK = 9,
     GRAB = 10,
 
+    SEARCH_RANDOM_ROOM = 11,
+    CHECK_PICKUP_BY_OTHER = 12,
+
 
 class BlindAgent(BW4TBrain):
 
@@ -41,17 +44,28 @@ class BlindAgent(BW4TBrain):
         self._navigator = Navigator(agent_id=self.agent_id,
                                     action_set=self.action_set, algorithm=Navigator.A_STAR_ALGORITHM)
         self._objects = []
-        self._goal_blocks = []                      # Universally known goal blocks
-        self._goal_blocks_locations = []            # Goal block locations as said by other agents
-        self._goal_blocks_locations_followed = []   # Goal block locations already followed
+        self._goal_blocks = []                              # Universally known goal blocks
+        self._goal_blocks_locations = []                    # Goal block locations as said by other agents
+        self._goal_blocks_locations_followed = []           # Goal block locations already followed (by blind)
+        self._goal_blocks_location_followed_by_others = []  # Goal block locations where other agents went (from messages)
+
         self._trustBeliefs = []
 
         self._current_obj = None
+        self._is_color_blind = True
+        self._drop_location_blind = None
 
     def filter_bw4t_observations(self, state):
+        for obj in state:
+            if "is_collectable" in state[obj] and state[obj]['is_collectable']:
+                state[obj]['visualization'].pop('colour')
+                # or put none
+                # state[obj]['visualization']['colour'] = None
         return state
 
     def decide_on_bw4t_action(self, state: State):
+        state = self.filter_bw4t_observations(state)
+
         agent_name = state[self.agent_id]['obj_id']
         # Add team members
         for member in state['World']['team_members']:
@@ -65,6 +79,8 @@ class BlindAgent(BW4TBrain):
 
         # Remember locations of goal blocks (as said by others in messages)
         self._findGoalBlocksInMessages(receivedMessages, self._teamMembers)
+        # And remove picked up locations
+        self._removePickedUpGoalBlocks(receivedMessages)
 
         while True:
             if Phase.PLAN_PATH_TO_CLOSED_DOOR == self._phase:
@@ -72,15 +88,26 @@ class BlindAgent(BW4TBrain):
                 closedDoors = [door for door in state.values()
                                if 'class_inheritance' in door and 'Door' in door['class_inheritance'] and not door[
                         'is_open']]
+
+                # If no more closed doors
                 if len(closedDoors) == 0:
-                    return None, {}
+                    # Go check if others picked up goal blocks when they said they did
+                    if len(self._goal_blocks_location_followed_by_others) > 0:
+                        self._phase = Phase.CHECK_PICKUP_BY_OTHER
+                        return None, {}
+                    # Else
+                    else:
+                        self._phase = Phase.SEARCH_RANDOM_ROOM
+                        return None, {}
+
                 # Randomly pick a closed door
                 self._door = random.choice(closedDoors)
                 doorLoc = self._door['location']
                 # Location in front of door is south from door
                 doorLoc = doorLoc[0], doorLoc[1] + 1
                 # Send message of current action
-                self._sendMessage('Moving to ' + self._door['room_name'], agent_name)
+                self.moveToMessage(agent_name)
+
                 self._navigator.add_waypoints([doorLoc])
                 self._phase = Phase.FOLLOW_PATH_TO_CLOSED_DOOR
 
@@ -109,14 +136,13 @@ class BlindAgent(BW4TBrain):
                 is_open = state.get_room_doors(self._door['room_name'])[0]['is_open']
 
                 if not is_open:
-                    self._sendMessage("Opening door of " + self._door['room_name'], agent_name)
+                    self.openingDoorMessage(agent_name)
                     self._phase = Phase.SEARCH_ROOM
-                    self._sendMessage("Searching through " + self._door['room_name'], agent_name)
+                    self.searchingThroughMessage(agent_name)
                     return OpenDoorAction.__name__, {'object_id': self._door['obj_id']}
                 else:
-                    self._sendMessage("Searching through " + self._door['room_name'], agent_name)
+                    self.searchingThroughMessage(agent_name)
                     self._phase = Phase.SEARCH_ROOM
-
 
             if Phase.SEARCH_ROOM == self._phase:
                 self._state_tracker.update(state)
@@ -127,10 +153,7 @@ class BlindAgent(BW4TBrain):
                     if "Block" in c['name']:
                         if c not in self._objects:
                             self._objects.append(c)
-
-                            message = "Found block {\"size\": " + str(c['visualization']['size']) + ", \"shape\": " + \
-                                      str(c['visualization']['shape']) + "} at location " + str(c['location'])
-                            self._sendMessage(message, agent_name)
+                            self.foundBlockMessage(c, agent_name)
 
                 action = self._navigator.get_move_action(self._state_tracker)
                 if action is not None:
@@ -142,6 +165,7 @@ class BlindAgent(BW4TBrain):
             if Phase.CHECK_GOAL_TO_FOLLOW == self._phase:
                 follow = None
                 for loc in self._goal_blocks_locations:
+                    # If blind didn't follow location & there is no picking up at that location go follow
                     if loc not in self._goal_blocks_locations_followed:
                         follow = loc['location']
                         self._goal_blocks_locations_followed.append(loc)
@@ -176,25 +200,49 @@ class BlindAgent(BW4TBrain):
                 # If object is still there
                 if len(l) > 0:
                     self._current_obj = l[0]
-
-                    message = "Picking up block {\"size\": " + str(self._current_obj['visualization']['size']) + ", \"shape\": " + \
-                                  str(self._current_obj['visualization']['shape']) + "} at location " + str(self._current_obj['location'])
-                    self._sendMessage(message, agent_name)
+                    self.pickingUpBlockSimpleMessage(self._current_obj, agent_name)
 
                     self._phase = Phase.GRAB
                     return GrabObject.__name__, {'object_id': self._current_obj['obj_id']}
-                # Else (if another agent has picked up the block)
                 else:
+                    # TODO: Either another agent has picked up block or has lied about finding block here
+                    self._phase = Phase.CHECK_GOAL_TO_FOLLOW
+
+            if Phase.CHECK_PICKUP_BY_OTHER == self._phase:
+                self._state_tracker.update(state)
+                action = self._navigator.get_move_action(self._state_tracker)
+
+                if action is not None:
+                    return action, {}
+
+                location_check = self._goal_blocks_location_followed_by_others[0]['location']
+                objs_in_area = state.get_objects_in_area(location_check, 1, 1)
+                l = list(filter(lambda obj: 'Block' in obj['name'] and obj['location'] == location_check, objs_in_area))
+                # If object is still there
+                if len(l) > 0:
+                    # TODO: Here decrease trust for teamMember ?
+                    # And then pick up and move it yourself
+                    self._current_obj = l[0]
+                    self.pickingUpBlockSimpleMessage(self._current_obj, agent_name)
+
+                    self._phase = Phase.GRAB
+                    return GrabObject.__name__, {'object_id': self._current_obj['obj_id']}
+
+                # If object is not there anymore
+                else:
+                    # TODO: Here increase trust for teamMember ?
                     self._phase = Phase.CHECK_GOAL_TO_FOLLOW
 
 
             if Phase.GRAB == self._phase:
                 self._navigator.reset_full()
 
-                # Get location between door and drop zone
-                loc = self._goal_blocks[2]['location']
-                above_drop_location = (loc[0], loc[1] - 1)
-                self._navigator.add_waypoints([above_drop_location])
+                # Blind drops object between drop zone and door of closest room
+                if self._drop_location_blind is None:
+                    loc = self._goal_blocks[2]['location']
+                    self._drop_location_blind = (loc[0], loc[1] - 1)
+
+                self._navigator.add_waypoints([self._drop_location_blind])
 
                 self._phase = Phase.MOVING_BLOCK
 
@@ -206,13 +254,23 @@ class BlindAgent(BW4TBrain):
                     return action, {}
 
                 if state[agent_name]['is_carrying']:
-                    message = "Dropped block {\"size\": " + str(self._current_obj['visualization']['size']) + ", \"shape\": " + \
-                              str(self._current_obj['visualization']['shape']) + "} at location " + str(self._current_obj['location'])
-                    self._sendMessage(message, agent_name)
+                    self.droppingBlockSimpleMessage(self._current_obj, self._drop_location_blind, agent_name)
                     return DropObject.__name__, {'object_id': self._current_obj['obj_id']}
 
                 # After droping block, check if there is another goal to follow
                 self._phase = Phase.CHECK_GOAL_TO_FOLLOW
+
+            if Phase.SEARCH_RANDOM_ROOM == self._phase:
+                doors = [door for door in state.values()
+                         if 'class_inheritance' in door and 'Door' in door['class_inheritance']]
+                self._door = random.choice(doors)
+                doorLoc = self._door['location']
+                # Location in front of door is south from door
+                doorLoc = doorLoc[0], doorLoc[1] + 1
+                # Send message of current action
+                self.moveToMessage(agent_name)
+                self._navigator.add_waypoints([doorLoc])
+                self._phase = Phase.FOLLOW_PATH_TO_CLOSED_DOOR
 
 
     def _sendMessage(self, mssg, sender):
@@ -236,17 +294,20 @@ class BlindAgent(BW4TBrain):
                     receivedMessages[member].append(mssg.content)
         return receivedMessages
 
-    def checkMessagePickingUpBlock(self, receivedMessages, teamMembers, location):
-        for member in teamMembers:
+    def _removePickedUpGoalBlocks(self, receivedMessages):
+        for member in self._teamMembers:
             messages = receivedMessages[member]
             for mess in messages:
                 if mess[0:21] == "Picking up goal block":
                     loc_str = mess[mess.find("location") + 9:len(mess)]
                     location_goal = (int(loc_str[1:loc_str.find(",")]), int(loc_str[loc_str.find(",") + 2:len(loc_str) - 1]))
-                    if location == location_goal:
-                        return True
-        return False
+                    for loc in self._goal_blocks_locations:
+                        if loc['location'] == location_goal:
+                            self._goal_blocks_locations.remove(loc)
 
+                            self._goal_blocks_location_followed_by_others.add(loc)
+                            # Sort locations followed ascending on trust level
+                            self._goal_blocks_location_followed_by_others.sort(key=lambda x: x['trustLevel'])
 
     def _findGoalBlocksInMessages(self, receivedMessages, teamMembers):
         # If another agent found goal block, check other attributes that we know and go pick it up
@@ -273,9 +334,8 @@ class BlindAgent(BW4TBrain):
                             }
                             if obj not in self._goal_blocks_locations:
                                 self._goal_blocks_locations.append(obj)
+                                # Sort by trust (follow first locations from most trusted team members)
                                 self._goal_blocks_locations.sort(key=lambda x: x['trustLevel'], reverse=True)
-
-
 
     def updateGoalBlocks(self, state):
         if len(self._goal_blocks) == 0:
@@ -297,3 +357,33 @@ class BlindAgent(BW4TBrain):
                     trustBeliefs[member] -= 0.1
                     break
         return trustBeliefs
+
+    def moveToMessage(self, agent_name):
+        self._sendMessage('Moving to ' + self._door['room_name'], agent_name)
+
+    def openingDoorMessage(self, agent_name):
+        self._sendMessage('Opening door of ' + self._door['room_name'], agent_name)
+
+    def searchingThroughMessage(self, agent_name):
+        self._sendMessage('Searching through ' + self._door['room_name'], agent_name)
+
+    def foundBlockMessage(self, data, agent_name):
+        get_with_color = 2 if self._is_color_blind else 3
+        item_info = dict(list(data['visualization'].items())[:get_with_color])
+        self._sendMessage(
+            "Found block: " + json.dumps(item_info) +
+            " at location (" + ', '.join([str(loc) for loc in data['location']]) + ")", agent_name)
+
+    def pickingUpBlockSimpleMessage(self, data, agent_name):
+        get_with_color = 2 if self._is_color_blind else 3
+        item_info = dict(list(data['visualization'].items())[:get_with_color])
+        self._sendMessage(
+            "Picking up block: " + json.dumps(item_info) +
+            " at location (" + ', '.join([str(loc) for loc in data['location']]) + ")", agent_name)
+
+    def droppingBlockSimpleMessage(self, data, location, agent_name):
+        get_with_color = 2 if self._is_color_blind else 3
+        item_info = dict(list(data['visualization'].items())[:get_with_color])
+        self._sendMessage(
+            "Dropped block: " + json.dumps(item_info) +
+            " at location (" + ', '.join([str(loc) for loc in location]) + ")", agent_name)
