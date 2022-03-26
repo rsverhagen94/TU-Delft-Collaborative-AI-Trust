6c37dbf6299d88a2d1e4e99b913f6607a90f9512
Ana Vasilcoiu <a.m.vasilcoiu@student.tudelft.nl>
2022-03-26 16:19:32 +0100

    Change trust based on each visualisation elem

--- agents1/BlindAgent.py
+++ agents1/BlindAgent.py
@@ -403,13 +403,28 @@ class BlindAgent(BW4TBrain):
     def dropGoalBlockUpdate(self, block, member):
         return
 
-
     def updateGoalBlocks(self, state):
         if len(self._goal_blocks) == 0:
             self._goal_blocks = [goal for goal in state.values()
                         if 'is_goal_block' in goal and goal['is_goal_block']]
 
-    def _trustBlief(self, members, received, state, close_objects):
+    def check_same_visualizations(self, vis1, vis2):
+        size = 0
+        shape = 0
+        colour = 0
+
+        if "size" in vis1 and "size" in vis2:
+            size = 0.033 if vis1['size'] == vis2['size'] else -0.033
+
+        if "shape" in vis1 and "shape" in vis2:
+            shape = 0.033 if vis1['shape'] == vis2['shape'] else -0.033
+
+        if "colour" in vis1 and "colour" in vis2:
+            colour = 0.033 if vis1['colour'] == vis2['colour'] else -0.033
+
+        return size + shape + colour
+
+    def _trustBlief(self, state, close_objects):
         '''
         Baseline implementation of a trust belief. Creates a dictionary with trust belief scores for each team member, for example based on the received messages.
         '''
@@ -427,25 +442,24 @@ class BlindAgent(BW4TBrain):
                     member = messages[-1]['memberName']
                     # If last message is 'pick-up' substract from trust
                     if messages[-1]['action'] == "pick-up":
-                        self._trust[member]['pick-up'] = max(round(self._trust[member]['pick-up'] - 0.1, 2), 0)
+                        self._trust[member]['pick-up'] = max(round(self._trust[member]['pick-up'] - 0.1, 3), 0)
                     # If last message is 'found' or 'drop-of' add to trust
                     if messages[-1]['action'] == "found" or messages[-1]['action'] == "drop-off":
-                        if o['visualization'] == messages[-1]['block']:
-                            self._trust[member]['found'] = min(round(self._trust[member]['found'] + 0.1, 2), 1)
+                        val = self.check_same_visualizations(o['visualization'], messages[-1]['block'])
+                        self._trust[member]['found'] = min(round(self._trust[member]['found'] + val, 3), 1)
                     if len(messages) > 1:
                         i = len(messages) - 2
                         while i >= 0:
                             member = messages[i]['memberName']
                             if messages[-1]['action'] == "drop-off":
-                                self._trust[member]['drop-off'] = min(round(self._trust[member]['drop-off'] + 0.1, 2),
-                                                                      1)
+                                self._trust[member]['drop-off'] = min(round(self._trust[member]['drop-off'] + 0.1, 3),1)
                                 break
                             if not messages[-1]['action'] == "found":
                                 break
-                            if o['visualization'] == messages[-1]['block']:
-                                self._trust[member]['found'] = min(round(self._trust[member]['found'] + 0.1, 2), 1)
-                            else:
-                                self._trust[member]['found'] = max(round(self._trust[member]['found'] - 0.1, 2), 0)
+
+                            val = self.check_same_visualizations(o['visualization'], messages[-1]['block'])
+                            self._trust[member]['found'] = min(round(self._trust[member]['found'] + val, 3), 1)
+
                             i -= 1
 
         agentLocation = state[self.agent_id]['location']
@@ -458,15 +472,14 @@ class BlindAgent(BW4TBrain):
                         'action'] == "drop-off":
                         if close_objects is None:
                             self._trust[member][messages[-1]['action']] = max(
-                                round(self._trust[member][messages[-1]['action']] - 0.1, 2), 0)
+                                round(self._trust[member][messages[-1]['action']] - 0.1, 3), 0)
                         else:
                             found = False
                             for o in close_objects:
                                 if o['location'] == (x, y):
-                                    if o['visualization'] == messages[-1]['block']:
-                                        found = True
+                                    found = True
                             if found is False:
                                 self._trust[member][messages[-1]['action']] = max(
-                                    round(self._trust[member][messages[-1]['action']] - 0.1, 2), 0)
+                                    round(self._trust[member][messages[-1]['action']] - 0.1, 3), 0)
 
 
